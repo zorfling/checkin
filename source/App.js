a8c9da7c6da3e03014a17a7701cbb268a08b7194
Chris Colborne <zorfling@gmail.com>
2015-10-27 14:13:16 +1000

    Something is working

--- source/App.js
+++ source/App.js
@@ -1,1 +1,9 @@
-export default React => ({ foo }) => (<div>Content goes here: { foo }</div>);
+import React from 'react';
+import createTitle from './title/index';
+const Title = createTitle(React);
+
+export default React => ({ ...props }) => (
+    <div>
+        Content goes here: { props.foo }
+        <Title { ...props}></Title>
+    </div>);
